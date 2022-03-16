bc201bee663309bada7ad2fc9f95ca5e36374f0e
vbalbino <valcir.balbinottijunior@nttdata.com>
2022-03-16 19:42:28 -0300

    Initial commit

---
+++ src/main/java/utfpr/edu/br/servidorweb/ServidorwebApplication.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.servidorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utfpr/edu/br/servidorweb/ServidorwebApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.servidorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
